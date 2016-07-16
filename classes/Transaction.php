73e1f8e8715b8da2e5a035ac0c871919e5c3eaf2
Dryke <rabinovitchlouis@gmail.com>
2016-07-16 18:52:06 +0200

    add class Transaction

---
+++ classes/Transaction.php
@@ -0,0 +1,12 @@
+<?php
+  class Transaction
+  {
+    public $id;
+    public $user_id;
+    public $category_id;
+    public $name;
+    public $amount;
+    public $sign;
+    public $date_add;
+  }
+?>
